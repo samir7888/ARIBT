06d7bcebe266ba1e29b8ebda71fdea86b5e4fd73
Sameer Basnet <basnetsameer78@gmail.com>
2025-05-20 14:23:28 +0545

    fix: update DOMPurify import to isomorphic-dompurify for consistency across components; add isomorphic-dompurify to dependencies

--- app/components/cards/TestimonialBody.tsx
+++ app/components/cards/TestimonialBody.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import DOMPurify from "dompurify";
+import DOMPurify from "isomorphic-dompurify";
 
 
 const TestimonialBody = ({ description }: { description: string }) => {
--- app/services/components/ServicesBody.tsx
+++ app/services/components/ServicesBody.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import DOMPurify from "dompurify";
+import DOMPurify from "isomorphic-dompurify";
 
 
 const ServiceBody = ({ description }: { description: string }) => {
